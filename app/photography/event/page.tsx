4f7c13cb1b97c8464750ea2ba14b7cda7f22337e
Amir Berenji <a.berenji62@gmail.com>
2025-10-15 20:08:03 +0400

    Change the Comming Soon part

--- app/photography/event/page.tsx
+++ app/photography/event/page.tsx
@@ -1,9 +1,13 @@
 import CommingSoonPage from "@/app/component/commingsoon";
+import EventImagePage from "./evantimage";
 
 export default function Page() {
   return (
     <>
+    <section className="" >
       <CommingSoonPage />
+    </section>
+      
     </>
   );
 }
